afa313297d68347a469e666ffdc2adb6b2647501
jbernard3853 <32273189+jbernard3853@users.noreply.github.com>
2017-10-31 10:49:11 -0400

    Update Deli.java

--- TextBasedProject/src/Building/Deli.java
+++ TextBasedProject/src/Building/Deli.java
@@ -1,9 +1,19 @@
 package Building;
 
 public class Deli extends Building{
-public void print() {
-		
-		System.out.print("You've ran into a deli! \n You can purchase: \n");
-
-	}
+String[]menu = {};
+//int sandwichType = random number between 1 and 7;
+private void generateMenu()
+{
+	String[] hotSandwich = {"Bacon Egg and Cheese","BLT with Chipotle Mayo","Toasted Bagel with Butter"};
+	//add two or three random sandwiches to the menu;
+}
+public String getMenu()
+{
+	
+}
+public void print() 
+{
+	System.out.print("You've ran into a deli! \n You can purchase: \n");
+}
 }
